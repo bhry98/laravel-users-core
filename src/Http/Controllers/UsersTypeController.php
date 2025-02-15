5bda5890b518e7de8a9eecfc8cf8443029be4217
bhry98 <abdelrahman.mahmoud@valleysoft-eg.com>
2025-02-15 13:46:26 +0200

    fix some issues

--- src/Http/Controllers/UsersTypeController.php
+++ src/Http/Controllers/UsersTypeController.php
@@ -4,15 +4,16 @@ namespace Bhry98\LaravelUsersCore\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Bhry98\LaravelUsersCore\Http\Requests\types\GetAllCountriesRequest;
+use Bhry98\LaravelUsersCore\Http\Requests\types\GetAllTypesRequest;
 use Bhry98\LaravelUsersCore\Http\Resources\TypeResource;
 use Bhry98\LaravelUsersCore\Services\UsersCoreTypesService;
 
 class UsersTypeController extends Controller
 {
-    function getAll(GetAllCountriesRequest $request, UsersCoreTypesService $usersTypesService): \Illuminate\Http\JsonResponse
+    function getAll(GetAllTypesRequest $request, UsersCoreTypesService $usersTypesService): \Illuminate\Http\JsonResponse
     {
         try {
-            $typeData = $usersTypesService->getAllTypes($request->perPage,$request->searchForWord);
+            $typeData = $usersTypesService->getAllTypes($request->perPage, $request->searchForWord);
             if ($typeData) {
                 return bhry98_response_success_with_data(TypeResource::collection($typeData)->response()->getData(true));
             } else {
